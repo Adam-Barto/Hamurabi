0c31e57d9a846a2ff56800ac1b03c5e8cae69a8e
Adam-Barto <adam.jbarto@gmail.com>
2023-03-17 16:19:28 -0400

    Base code Upload

---
+++ docs/Hammurabi.py
@@ -0,0 +1,13 @@
+import random
+
+
+
+class Hamurabi(object):
+    def play_game(self):
+        print
+        "Let's play!"
+    ## lots more methods here...
+
+
+if __name__ == '__main__':
+    Hamurabi().play_game()
